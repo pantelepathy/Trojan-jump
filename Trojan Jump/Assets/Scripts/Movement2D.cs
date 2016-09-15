e8db002c83d2a8237d0d60781ecbd0c001ba43b0
PanJinfeng <panjinfeng@PanJinfengdeMacBook-Pro.local>
2016-09-14 17:59:11 -0700

    player with left/right movement, wrap-round and bounce-up

---
+++ Trojan Jump/Assets/Scripts/Movement2D.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement2D : MonoBehaviour {
+
+	public float moveSpeed = 4f;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float h = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (h * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+}
